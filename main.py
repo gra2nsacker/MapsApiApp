78473e646a7e7668126e15b5b48e4666d7833ce1
gra2nsacker <gmaegoriy@gmail.com>
2022-02-12 14:10:03 +0300

    add QT basse

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    pass
+import sys
+
+import PyQt5
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        # uic.loadUi("../UI/Menu.ui", self)
+        self.maximumWidth()
+        self.setMaximumSize(950, 720)
+        self.setMinimumSize(950, 720)
+        self.setWindowTitle("MENU")
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = MainWindow()
+    ex.show()
+    sys.exit(app.exec_())
