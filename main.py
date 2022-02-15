0fb23c90e1e3a874ef6b8a9d8aadc3bd5254118d
LyashenkoAndron <lyashenko.andron@mail.ru>
2022-02-15 19:29:25 +0300

    2nd task

--- main.py
+++ main.py
@@ -1,21 +1,121 @@
+import pygame
+import requests
 import sys
+import os
+import math
+from dgj_podskazka.distance import lonlat_distance
+from dgj_podskazka.geo import reverse_geocode
+from dgj_podskazka.bis import find_business
 
-import PyQt5
-from PyQt5.QtWidgets import QMainWindow, QApplication
+LAT_STEP = 0.008
+LON_STEP = 0.002
+coord_to_geo_x = 0.0000428
+coord_to_geo_y = 0.0000428
 
 
-class MainWindow(QMainWindow):
+def ll(x, y):
+    return "{0},{1}".format(x, y)
+
+
+class SearchResult(object):
+
+    def __init__(self, point, address, postal_code=None):
+        self.point = point
+        self.address = address
+        self.postal_code = postal_code
+
+
+class MapParams(object):
     def __init__(self):
-        super().__init__()
-        # uic.loadUi("../UI/Menu.ui", self)
-        self.maximumWidth()
-        self.setMaximumSize(950, 720)
-        self.setMinimumSize(950, 720)
-        self.setWindowTitle("MENU")
+        self.lat = 55.729738
+        self.lon = 37.664777
+        self.zoom = 15
+        self.type = "map"
+
+        self.search_result = None
+        self.use_postal_code = False
+
+    def ll(self):
+        return ll(self.lon, self.lat)
+
+    def update(self, event):
+        if event.key == pygame.K_p and self.zoom < 19:
+            self.zoom += 1
+        elif event.key == pygame.K_m and self.zoom > 2:
+            self.zoom -= 1
+
+        if self.lon > 180: self.lon -= 360
+        if self.lon < -180: self.lon += 360
+
+    def screen_to_geo(self, pos):
+        dy = 225 - pos[1]
+        dx = pos[0] - 300
+        lx = self.lon + dx * coord_to_geo_x * math.pow(2, 15 - self.zoom)
+        ly = self.lat + dy * coord_to_geo_y * math.cos(math.radians(self.lat)) * math.pow(2,
+                                                                                          15 - self.zoom)
+        return lx, ly
+
+    def add_reverse_toponym_search(self, pos):
+        point = self.screen_to_geo(pos)
+        toponym = reverse_geocode(ll(point[0], point[1]))
+        self.search_result = SearchResult(
+            point,
+            toponym["metaDataProperty"]["GeocoderMetaData"]["text"] if toponym else None,
+            toponym["metaDataProperty"]["GeocoderMetaData"]["Address"].get(
+                "postal_code") if toponym else None)
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MainWindow()
-    ex.show()
-    sys.exit(app.exec_())
+def load_map(mp):
+    map_request = "http://static-maps.yandex.ru/1.x/?ll={ll}&z={z}&l={type}".format(ll=mp.ll(),
+                                                                                    z=mp.zoom,
+                                                                                    type=mp.type)
+    if mp.search_result:
+        map_request += "&pt={0},{1},pm2grm".format(mp.search_result.point[0],
+                                                   mp.search_result.point[1])
+
+    response = requests.get(map_request)
+    if not response:
+        sys.exit(1)
+    map_file = "map.png"
+    try:
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+    except IOError as ex:
+        print("Ошибка записи временного файла:", ex)
+        sys.exit(2)
+
+    return map_file
+
+
+def render_text(text):
+    font = pygame.font.Font(None, 30)
+    return font.render(text, 1, (100, 0, 100))
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+    mp = MapParams()
+
+    while True:
+        event = pygame.event.wait()
+        if event.type == pygame.QUIT:
+            break
+        elif event.type == pygame.KEYUP:
+            mp.update(event)
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                mp.add_reverse_toponym_search(event.pos)
+        else:
+            continue
+
+        map_file = load_map(mp)
+        screen.blit(pygame.image.load(map_file), (0, 0))
+        pygame.display.flip()
+
+    pygame.quit()
+    os.remove(map_file)
+
+
+if __name__ == "__main__":
+    main()
